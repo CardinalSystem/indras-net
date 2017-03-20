ae612b352ab88471b97bf51793751a4b8fe3ab7d
Jirawat I <nodtem66@gmail.com>
2017-03-20 23:31:52 +0700

    fixed invalid api

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ Since: 18 Mar. 2017
 import os
 from wit import Wit
 from actions import ACTIONS
-from flask import Flask, request, abort
+from flask import Flask, request, abort, g
 
 from linebot import (
     LineBotApi, WebhookParser
@@ -29,6 +29,7 @@ line_bot_api = LineBotApi(LINE_TOKEN)
 wit_client = Wit(access_token=WIT_TOKEN, actions=ACTIONS)
 parser = WebhookParser(LINE_SECRET)
 
+g._context = {}
 
 @app.route("/line", methods=['POST'])
 def callback():
@@ -40,6 +41,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
+    context = getattr(g, '_context', {})
     app.logger.info("Request body: " + body)
 
     # handle webhook body
@@ -57,10 +59,11 @@ def callback():
         if event.reply_token == 'f'*32 or event.reply_token == '0'*32:
             continue
         # send to wit.ai
-        response = wit_client.message(event.message.text)
+        context = wit_client.run_actions('', event.message.text, context)
+        app.logger.info(context)
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=response.decode('utf-8'))
+            TextSendMessage(text=str(context))
         )
 
     return 'OK'
@@ -70,8 +73,8 @@ def hello_word():
     """
     Route for helloworld echo
     """
+    app.logger.info('WIT_TOKEN: {0}\nLINE_TOKEN: {1}'.format(WIT_TOKEN, LINE_TOKEN))
     return "Hello world"
 
 if __name__ == "__main__":
-    print('WIT_TOKEN: {0}\nLINE_TOKEN: {1}'.format(WIT_TOKEN, LINE_TOKEN))
     app.run(host="0.0.0.0", port=8000, debug=True)
